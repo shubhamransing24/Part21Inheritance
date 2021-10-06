a9e71c39ef3ebe4644db62c2a86725490daca9ce
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-06 12:11:34 +0530

    Add project files.

---
+++ Part21Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Part21Inheritance
+{
+
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        //string Gender;
+        //we use inheritance becouse of reusablity perpose
+        //why to create firstname and lastname again and again in child classes use parent class fields
+        public void PrintFullName()
+        {
+            Console.WriteLine("Full Name Of The Employee:{0} {1}",FirstName,LastName);
+
+        }
+
+    }
+
+
+   public class FullTimeEmployee : Employee
+    {
+        int YearlySalary = 400000;
+
+    }
+   public class PartTimeEmployee :Employee
+    {
+        int HourlySalary = 400;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee fte = new FullTimeEmployee();
+            fte.FirstName = "Shubham";
+            fte.LastName = "Ransing";
+            PartTimeEmployee pte = new PartTimeEmployee();
+            pte.FirstName = "Saurabh";
+            pte.LastName = "Ransing"; ;
+            fte.PrintFullName();
+            pte.PrintFullName();
+            
+        }
+    }
+}
